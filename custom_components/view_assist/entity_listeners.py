302158164f1126856cb8e867c7f1289ae8727e02
Donny F <donny911@hotmail.com>
2025-02-07 12:10:09 -0600

    figuring out watch states for state and attribute

--- custom_components/view_assist/entity_listeners.py
+++ custom_components/view_assist/entity_listeners.py
@@ -17,14 +17,31 @@ class EntityListeners:
         self.hass = hass
         self.config_entry = config_entry
 
-        mic_device = self.config_entry.data["mic_device"]
-
+        # mic_device = self.config_entry.data["mic_device"]
+        mic_device = "input_boolean.test"
+        #mediaplayer_device = "media_player.viewassist_officetsv"
+        mediaplayer_device = self.config_entry.data["mediaplayer_device"]
+        
         # Add mic listener
         config_entry.async_on_unload(
             async_track_state_change_event(hass, mic_device, self._async_on_mic_change)
         )
 
+        # Add media player mute listener
+        config_entry.async_on_unload(
+            async_track_state_change_event(hass, mediaplayer_device, self._async_on_mediaplayer_device_mute_change)
+        )
+
     @callback
     def _async_on_mic_change(self, event: Event[EventStateChangedData]) -> None:
+        old_state = event.data["old_state"]
         new_state = event.data["new_state"]
-        _LOGGER.info("STATE CHANGE: %s", new_state)
+        _LOGGER.info("OLD STATE: %s", old_state.state)
+        _LOGGER.info("NEW STATE: %s", new_state.state)
+
+    @callback
+    def _async_on_mediaplayer_device_mute_change(self, event: Event[EventStateChangedData]) -> None:
+        old_state = event.data["old_state"]
+        new_state = event.data["new_state"]
+        _LOGGER.info("OLD STATE: %s", old_state.attributes['is_volume_muted'])
+        _LOGGER.info("NEW STATE: %s", new_state.attributes['is_volume_muted'])
