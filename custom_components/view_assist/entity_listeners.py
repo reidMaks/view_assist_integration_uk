ebc34f774f0ff33170d9b4a2f44afefd5c193824
Donny F <donny911@hotmail.com>
2025-02-09 16:47:57 -0600

    Add mic mute watcher

--- custom_components/view_assist/entity_listeners.py
+++ custom_components/view_assist/entity_listeners.py
@@ -19,20 +19,18 @@ class EntityListeners:
         self.hass = hass
         self.config_entry = config_entry
 
-        mic_device = self.config_entry.data["mic_device"]
-        mic_type = self.config_entry.options.get(
-            "mic_type", "Home Assistant Voice Satellite"
-        )
+        # Add microphone mute switch listener
+        mic_device = config_entry.runtime_data.mic_device
+        mic_type = config_entry.runtime_data.mic_type
         mute_switch = self.get_mute_switch(mic_device, mic_type)
 
-        mediaplayer_device = self.config_entry.data["mediaplayer_device"]
-
-        # Add microphone mute switch listener
         config_entry.async_on_unload(
             async_track_state_change_event(hass, mute_switch, self._async_on_mic_change)
         )
 
         # Add media player mute listener
+        mediaplayer_device = self.config_entry.data["mediaplayer_device"]
+
         config_entry.async_on_unload(
             async_track_state_change_event(
                 hass, mediaplayer_device, self._async_on_mediaplayer_device_mute_change
@@ -45,24 +43,29 @@ class EntityListeners:
             self.hass, f"{DOMAIN}_{self.config_entry.entry_id}_update"
         )
 
-    # @callback
-    # def _async_on_mic_change(self, event: Event[EventStateChangedData]) -> None:
-    #     old_state = event.data["old_state"]
-    #     new_state = event.data["new_state"]
-    #     _LOGGER.info("OLD STATE: %s", old_state.state)
-    #     _LOGGER.info("NEW STATE: %s", new_state.state)
-    #     self.config_entry.runtime_data.do_not_disturb = new_state.state == "on"
-    #     if new_state.state == "on":
-    #         if "mic" not in self.config_entry.runtime_data.status_icons:
-    #             self.config_entry.runtime_data.status_icons.append("mic")
-    #     self.update_entity()
 
     @callback
     def _async_on_mic_change(self, event: Event[EventStateChangedData]) -> None:
-        old_state = event.data["old_state"]
-        new_state = event.data["new_state"]
-        _LOGGER.info("OLD STATE: %s", old_state.state)
-        _LOGGER.info("NEW STATE: %s", new_state.state)
+        mic_mute_new_state = event.data["new_state"].state
+
+        # If not change to mic mute state, exit function
+        if (
+            not event.data.get("old_state")
+            or event.data["old_state"].state
+            == mic_mute_new_state
+        ):
+            return
+
+        _LOGGER.info("MIC MUTE: %s", mic_mute_new_state)
+        status_icons = self.config_entry.runtime_data.status_icons.copy()
+
+        if mic_mute_new_state == "on" and "mic" not in status_icons:
+            status_icons.append("mic")
+        elif mic_mute_new_state == "off" and "mic" in status_icons:
+            status_icons.remove("mic")
+
+        self.config_entry.runtime_data.status_icons = status_icons
+        self.update_entity()        
 
     @callback
     def _async_on_mediaplayer_device_mute_change(
