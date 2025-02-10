bcfa9db931a405a6b6ac2e6f80d4cf2b8b2e8afd
Mark Parker <msparker@sky.com>
2025-02-10 16:53:45 +0000

    monitoring for attribute changes via set_state service

--- custom_components/view_assist/entity_listeners.py
+++ custom_components/view_assist/entity_listeners.py
@@ -1,12 +1,13 @@
 """Handles entity listeners."""
 
 import logging
+from typing import Any
 
 from homeassistant.core import Event, EventStateChangedData, HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.helpers.event import async_track_state_change_event
 
-from .const import DOMAIN, VAConfigEntry
+from .const import CONF_DO_NOT_DISTURB, DOMAIN, VA_ATTRIBUTE_UPDATE_EVENT, VAConfigEntry
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,6 +25,12 @@ class EntityListeners:
         mic_type = config_entry.runtime_data.mic_type
         mute_switch = self.get_mute_switch(mic_device, mic_type)
 
+        # Add listener to set_state service to call sensor_attribute_changed
+        hass.bus.async_listen(
+            VA_ATTRIBUTE_UPDATE_EVENT.format(config_entry.entry_id),
+            self.set_state_changed_attribute,
+        )
+
         config_entry.async_on_unload(
             async_track_state_change_event(hass, mute_switch, self._async_on_mic_change)
         )
@@ -37,20 +44,15 @@ class EntityListeners:
             )
         )
 
-        # Add do not disturb listener
-        dnd_device = "sensor." + config_entry.runtime_data.name
-
-        # config_entry.async_on_unload(
-        #    async_track_state_change_event(
-        #        hass, dnd_device, self._async_on_dnd_device_state_change
-        #    )
-        # )
-
     def update_entity(self):
         """Dispatch message that entity is listening for to update."""
         async_dispatcher_send(
             self.hass, f"{DOMAIN}_{self.config_entry.entry_id}_update"
         )
+
+    # ---------------------------------------------------------------------------------------
+    # Actions for monitoring changes to external entities
+    # ---------------------------------------------------------------------------------------
 
     @callback
     def _async_on_mic_change(self, event: Event[EventStateChangedData]) -> None:
@@ -105,30 +107,6 @@ class EntityListeners:
         self.config_entry.runtime_data.status_icons = status_icons
         self.update_entity()
 
-    @callback
-    def _async_on_dnd_device_state_change(
-        self, event: Event[EventStateChangedData]
-    ) -> None:
-        dnd_new_state = event.data["new_state"].attributes.get("do_not_disturb", False)
-
-        # If not change to dnd state, exit function
-        if (
-            not event.data.get("old_state")
-            or event.data["old_state"].attributes.get("do_not_disturb") == dnd_new_state
-        ):
-            return
-
-        _LOGGER.info("DND STATE: %s", dnd_new_state)
-        status_icons = self.config_entry.runtime_data.status_icons.copy()
-
-        if dnd_new_state and "dnd" not in status_icons:
-            status_icons.append("dnd")
-        elif not dnd_new_state and "dnd" in status_icons:
-            status_icons.remove("dnd")
-
-        self.config_entry.runtime_data.status_icons = status_icons
-        self.update_entity()
-
     def get_mute_switch(self, target_device: str, mic_type: str):
         """Get mute switch."""
 
@@ -142,3 +120,45 @@ class EntityListeners:
             return target_device.replace("assist_satellite", "switch") + "_mute"
 
         return None
+
+    # ---------------------------------------------------------------------------------------
+    # Actions for attributes changed via the set_state service
+    # ---------------------------------------------------------------------------------------
+
+    @callback
+    def set_state_changed_attribute(self, event: Event):
+        """Call when a sensor attribute is changed by the set_state service."""
+
+        # This function is only called if the new_value != old_value so no need to test
+        # for that.  If you are here, an attribute has changed.
+        attribute = event.data.get("attribute")
+        old_value = event.data.get("old_value")
+        new_value = event.data.get("new_value")
+
+        _LOGGER.info(
+            "ATTR CHANGED: %s - old value: %s, new value: %s",
+            attribute,
+            old_value,
+            new_value,
+        )
+
+        if attribute == CONF_DO_NOT_DISTURB:
+            self._async_on_dnd_device_state_change(event)
+
+    @callback
+    def _async_on_dnd_device_state_change(self, event: Event) -> None:
+        """Set dnd status icon."""
+
+        # This is called from our set_service event listener and therefore event data is
+        # slightly different.  See set_state_changed_attribute above
+        dnd_new_state = event.data["new_value"]
+
+        _LOGGER.info("DND STATE: %s", dnd_new_state)
+        status_icons = self.config_entry.runtime_data.status_icons.copy()
+        if dnd_new_state and "dnd" not in status_icons:
+            status_icons.append("dnd")
+        elif not dnd_new_state and "dnd" in status_icons:
+            status_icons.remove("dnd")
+
+        self.config_entry.runtime_data.status_icons = status_icons
+        self.update_entity()
