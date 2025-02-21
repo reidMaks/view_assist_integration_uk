cc9cf5edea65571e627718d4429060bb92a2bd2d
Mark Parker <msparker@sky.com>
2025-02-21 16:27:14 +0000

    use announce for music assistant

--- custom_components/view_assist/alarm_repeater.py
+++ custom_components/view_assist/alarm_repeater.py
@@ -3,14 +3,17 @@
 import asyncio
 from dataclasses import dataclass
 import logging
+import time
+from typing import Any
 
-from .const import BROWSERMOD_DOMAIN
 from homeassistant.components.media_player import MediaPlayerEntity, MediaType
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity, entity_registry as er
 from homeassistant.helpers.entity_component import DATA_INSTANCES, EntityComponent
 from homeassistant.helpers.network import get_url
+
+from .const import BROWSERMOD_DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,6 +26,8 @@ class PlayingMedia:
     media_type: str = "music"
     media_position: float = 0
     volume: int = 0
+    player: Any | None = None
+    queue: Any | None = None
 
 
 class VAAlarmRepeater:
@@ -63,38 +68,106 @@ class VAAlarmRepeater:
         #    _LOGGER.warning("Not PLAYING! - %s", media_entity.state)
         #    return None
         data = None
-        # An integration may populate these properties - most don't seem to
-        if content_id := media_entity.media_content_id:
-            data = PlayingMedia(
-                media_content_id=content_id,
-                media_type=media_entity.media_content_type | "music",
-                media_position=media_entity.media_position,
-            )
 
+        # Hook into integration data to get currently playing media
         media_integration = media_entity.platform.platform_name
+
         _LOGGER.debug("MEDIA ENTITY PLATFORM: %s", media_integration)
         # Browermod
         if media_integration == BROWSERMOD_DOMAIN:
             _data = media_entity._data  # noqa: SLF001
-            _LOGGER.debug("MEDIA ENTITY DATA: %s", _data)
-            return PlayingMedia(
+            data = PlayingMedia(
                 media_content_id=_data["player"].get("src"),
                 media_type="music",
                 media_position=_data.get("player", {}).get("media_position", None),
                 volume=_data.get("player", {}).get("volume", 0),
             )
-
-        # Music Assistant
-        if media_integration == "mass":
-            data = media_entity.mass
         # HA Voice Satellite
         elif media_integration == "esphome":
-            data = media_entity._entry_data  # noqa: SLF001
-        else:
-            data = None
+            _LOGGER.debug("MUSIC ASSISTANT DATA: %s", media_entity._entry_data)  # noqa: SLF001
+
+        # An integration may populate these properties - most don't seem to
+        elif content_id := media_entity.media_content_id:
+            data = PlayingMedia(
+                media_content_id=content_id,
+                media_type=media_entity.media_content_type or "music",
+                media_position=media_entity.media_position,
+            )
 
         _LOGGER.debug("MEDIA PLAYER DATA: %s", data)
-        return None
+        return data
+
+    async def wait_for_state(
+        self,
+        media_entity: MediaPlayerEntity,
+        data_var: str,
+        attribute: str,
+        wanted_state: Any,
+        timeout: float = 10.0,
+    ) -> None:
+        """Wait for an object attribute to reach the given state."""
+        start_timestamp = time.time()
+        _LOGGER.debug("Waiting for %s to reach state %s", attribute, wanted_state)
+        try:
+            async with asyncio.timeout(timeout):
+                while (
+                    getattr(getattr(media_entity, data_var), attribute) != wanted_state
+                ):
+                    await asyncio.sleep(0.2)
+
+        except TimeoutError:
+            _LOGGER.debug(
+                "%s did not reach state %s within the timeout of %s seconds",
+                attribute,
+                wanted_state,
+                timeout,
+            )
+        except Exception as ex:  # noqa: BLE001
+            _LOGGER.error("ERROR: %s", ex)
+
+        elapsed_time = round(time.time() - start_timestamp, 2)
+        _LOGGER.debug(
+            "%s reached state %s within %s seconds",
+            attribute,
+            wanted_state,
+            elapsed_time,
+        )
+
+    async def repeat_announce(
+        self,
+        integration: str,
+        media_entity: MediaPlayerEntity,
+        media_url: str,
+        max_repeats: int = 0,
+    ):
+        """Repeat announcement using announce methods."""
+        _LOGGER.debug("Using announce for alarm")
+
+        if media_url.startswith("/"):
+            media_url = media_url.removeprefix("/")
+            media_url = f"{get_url(self.hass)}/{media_url}"
+
+        if integration == "music_assistant":
+            i = 1
+            while i <= max_repeats or max_repeats == 0:
+                try:
+                    _LOGGER.debug("Announcing iteration %s", i)
+                    await self.hass.services.async_call(
+                        "music_assistant",
+                        "play_announcement",
+                        service_data={"url": media_url},
+                        target={"entity_id": "media_player.snapweb_client"},
+                    )
+                    await asyncio.sleep(1)
+                    await self.wait_for_state(
+                        media_entity, "player", "announcement_in_progress", False
+                    )
+                    await asyncio.sleep(1)
+                    i += 1
+                except Exception as ex:  # noqa: BLE001
+                    _LOGGER.error("ERROR: %s", ex)
+                    return
+            _LOGGER.debug("Announce ended")
 
     async def repeat_media(
         self, entity_id: str, media_type: str, media_url: str, max_repeats: int = 0
@@ -102,7 +175,7 @@ class VAAlarmRepeater:
         """Repeat playing media file."""
         # Format media url
 
-        if not media_url.startswith("http"):
+        if media_url.startswith("/"):
             media_url = media_url.removeprefix("/")
             media_url = f"{get_url(self.hass)}/{media_url}"
 
@@ -128,13 +201,13 @@ class VAAlarmRepeater:
             entities = list(self.alarm_tasks.keys())
 
         for mp_entity_id in entities:
-            await self.hass.services.async_call(
-                "media_player", "media_stop", {"entity_id": mp_entity_id}
-            )
             if (
                 self.alarm_tasks.get(mp_entity_id)
                 and not self.alarm_tasks[mp_entity_id].done()
             ):
+                await self.hass.services.async_call(
+                    "media_player", "media_stop", {"entity_id": mp_entity_id}
+                )
                 self.alarm_tasks[mp_entity_id].cancel()
 
     async def alarm_sound(
@@ -150,38 +223,47 @@ class VAAlarmRepeater:
         # Get instance of media player
         media_entity: MediaPlayerEntity
         if media_entity := self._get_entity_from_entity_id(entity_id):
-            playing_media = self.get_currently_playing_media(media_entity)
+            media_integration = media_entity.platform.platform_name
 
-            # Sound alarm
-            self.alarm_tasks[entity_id] = self.config.async_create_background_task(
-                self.hass,
-                self.repeat_media(entity_id, media_type, media_url, max_repeats),
-                name=f"AlarmRepeat-{entity_id}",
-            )
+            if media_integration == "music_assistant":
+                self.alarm_tasks[entity_id] = self.config.async_create_background_task(
+                    self.hass,
+                    self.repeat_announce(media_integration, media_entity, media_url, 3),
+                    name=f"AnnounceRepeat-{entity_id}",
+                )
+            else:
+                playing_media = self.get_currently_playing_media(media_entity)
 
-            # Wait for alarm media task to finish
-            while not self.alarm_tasks[entity_id].done():
-                await asyncio.sleep(0.2)
-
-            if resume and playing_media:
-                _LOGGER.debug("Resuming media: %s", playing_media)
-                await self.hass.services.async_call(
-                    "media_player",
-                    "play_media",
-                    {
-                        "entity_id": entity_id,
-                        "media_content_type": playing_media.media_type,
-                        "media_content_id": playing_media.media_content_id,
-                    },
+                # Sound alarm
+                self.alarm_tasks[entity_id] = self.config.async_create_background_task(
+                    self.hass,
+                    self.repeat_media(entity_id, media_type, media_url, max_repeats),
+                    name=f"AlarmRepeat-{entity_id}",
                 )
 
-                if playing_media.media_position:
+                # Wait for alarm media task to finish
+                while not self.alarm_tasks[entity_id].done():
+                    await asyncio.sleep(0.2)
+
+                if resume and playing_media:
+                    _LOGGER.debug("Resuming media: %s", playing_media)
                     await self.hass.services.async_call(
                         "media_player",
-                        "media_seek",
+                        "play_media",
                         {
                             "entity_id": entity_id,
-                            "seek_position": playing_media.media_position,
+                            "media_content_type": playing_media.media_type,
+                            "media_content_id": playing_media.media_content_id,
                         },
                     )
+
+                    if playing_media.media_position:
+                        await self.hass.services.async_call(
+                            "media_player",
+                            "media_seek",
+                            {
+                                "entity_id": entity_id,
+                                "seek_position": playing_media.media_position,
+                            },
+                        )
         return {}
